e037aab09613251da73a135f9f70254d9cbdcdef
Fralog4 <flogullo.bio@yahoo.com>
2024-12-02 18:17:23 +0100

    bootstrap insert

--- src/main/java/com/project/projectCS50x/ProjectCs50xApplication.java
+++ src/main/java/com/project/projectCS50x/ProjectCs50xApplication.java
@@ -1,5 +1,7 @@
 package com.project.projectCS50x;
 
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.vaadin.flow.theme.Theme;
@@ -12,6 +14,8 @@ import java.time.Clock;
 @SpringBootApplication
 @Theme("my-theme")
 @Push
+@StyleSheet("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css")
+@JavaScript("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js")
 public class ProjectCs50xApplication implements AppShellConfigurator {
 
     @Bean
--- src/main/java/com/project/projectCS50x/views/ChatAIView.java
+++ src/main/java/com/project/projectCS50x/views/ChatAIView.java
@@ -2,8 +2,7 @@ package com.project.projectCS50x.views;
 
 import com.project.projectCS50x.controller.ChatController;
 import com.project.projectCS50x.service.MistralAIService;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.messages.MessageInput;
@@ -11,9 +10,9 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.springframework.messaging.Message;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
-
 
 @Route("/ai/generate")
 public class ChatAIView extends VerticalLayout {
@@ -34,10 +33,15 @@ public class ChatAIView extends VerticalLayout {
         this.mistralAIService = mistralAIService;
         add(new H1("Welcome to your new AI Powered Chat!"));
         messageInput = new MessageInput(event -> sendMessage(event.getValue()));
+        messageInput.getElement().setAttribute("class", "form-control");
         add(messageInput);
         add(messageHistoryLayout);
         messageHistoryLayout.setWidthFull();
         messageHistoryLayout.setSpacing(true);
+
+        Button testButton= new Button("Test");
+        testButton.getElement().setAttribute("class", "btn btn-dark");  //bootstrap test
+        add(testButton);
 
 
     }
@@ -50,19 +54,17 @@ public class ChatAIView extends VerticalLayout {
             Div userMessage = new Div();
             userMessage.setText(message);
             userMessage.getStyle().set("backgroundColor", "lightblue");
+            userMessage.getElement().setAttribute("class",  "form-control-sm");
             messageHistory.add(userMessage);
             messageHistoryLayout.add(userMessage);
             String aiResponse = chatController.generate(message).toString();
             Div aiMessage = new Div();
             aiMessage.setText(aiResponse);
             aiMessage.getStyle().set("backgroundColor", "lightgray");
+            aiMessage.getElement().setAttribute("class",  "form-control-sm");
             messageHistory.add(aiMessage);
             messageHistoryLayout.add(aiMessage);
             messageHistoryLayout.scrollIntoView();
-
-
-            //String url = "/ai/generate?message=" + message;
-            //UI.getCurrent().navigate(url);
         }
         else{
             System.err.println("Please enter a message");
